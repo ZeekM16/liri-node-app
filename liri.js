f6d5604ec21aef64cb39ba03d136ff16613baacc
Isaac Molina <isaacmolina.m16@gmail.com>
2019-08-18 20:23:56 -0500

    added moviethis and spotify functions

--- liri.js
+++ liri.js
@@ -18,40 +18,62 @@ function spotifyThisSong() {
         type: 'track',
         query: parameter,
     })
-    .then(function(response){
-        var spotifyResponse = response.tracks.items[0];
-        var bandName = spotifyResponse.album.artists[0].name;
-        
-        console.log(spotifyResponse.name);
-        
-        
-        // console.log("Artist: " + bandName);
-        // console.log("Song Name: " + songName);
-    })
-    .catch(function(error){
-        console.log(error);
-    })
+        .then(function (response) {
+            var spotifyResponse = response.tracks.items[0];
+            var bandName = spotifyResponse.album.artists[0].name;
+
+            console.log(spotifyResponse.name);
+
+
+            console.log("Artist: " + bandName);
+            console.log("Song Name: " + songName);
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
     // axios
-    //     .get('https://api.spotify.com/v1/search?q=' + "i want it that way")
-    //     .then(function (response) {
-    //         console.log(response);
-    //     })
-    //     .catch(function(error){
-    //         console.log(error);
-    //     })
+        // .get('https://api.spotify.com/v1/search?q=' + "i want it that way")
+        // .then(function (response) {
+        //     console.log(response);
+        // })
+        // .catch(function(error){
+        //     console.log(error);
+        // })
 }
 
 
 // ========= MOVIE ============
+function movieThis(movieName) {
+    if (movieName) {
+        movieName = 'Mr. Nobody';
+    }
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+}
+axios.get(queryUrl).then(
+    function (response) {
+        if (!movieName) {
+            movieName = 'Mr. Nobody';
+        } console.log(response.data)
+        
 
+
+    }
+);
+
+//========== Bands ============
+// function concertThis(artist) {
 
 
 switch (command) {
     case 'spotify-this-song':
         spotifyThisSong()
         break;
-    // case 'movie-this'
-        // movieThis()
+    case 'movie-this':
+        movieThis()
+        break;
+    case 'concert-this':
+        concertThis();
+        break;
     default:
         console.log('Could not run command');
         break;
